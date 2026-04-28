1b61be6da21757961175ea6c86cea6c743c4d6bc
domitillaizzo98 <126577046+domizzo98@users.noreply.github.com>
2026-04-28 23:34:00 +0200

    Fix setup_env path

--- scripts/train_cifar10_unet_progressive_mask.sh
+++ scripts/train_cifar10_unet_progressive_mask.sh
@@ -48,7 +48,7 @@ if [[ -n "${REFERENCE_DIR}" && "${REFERENCE_DIR}" != /* ]]; then
   REFERENCE_DIR="${REPO_ROOT}/${REFERENCE_DIR}"
 fi
 
-source setup_env.sh
+source $WORK/discrete-diffusion-guidance/setup_env.sh
 export NCCL_P2P_LEVEL=NVL
 export HYDRA_FULL_ERROR=1
 # Weights & Biases Config
